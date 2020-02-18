891fc200c97e70c7f7aaf814110bf97999f5498a
Pavan <pavan.jando.2016@live.rhul.ac.uk>
2020-02-18 21:13:36 +0000

    Creates 3 tickets.

--- Lindar/src/Strip.java
+++ Lindar/src/Strip.java
@@ -21,10 +21,10 @@ class Strip{
 
     private void generateTickets(){
         Ticket t1 = new Ticket(stripGrid);
-        ticketStrip.add(t1);
-        /*Ticket t2 = new Ticket(t1.getRemainingNumbers());
+        Ticket t2 = new Ticket(t1.getRemainingNumbers());
         Ticket t3 = new Ticket(t2.getRemainingNumbers());
-        Ticket t4 = new Ticket(t3.getRemainingNumbers());
+        ticketStrip.addAll(Arrays.asList(t1, t2 ,t3));
+        /*Ticket t4 = new Ticket(t3.getRemainingNumbers());
         Ticket t5 = new Ticket(t4.getRemainingNumbers());
         Ticket t6 = new Ticket(t5.getRemainingNumbers());
         ticketStrip.addAll(Arrays.asList(t1, t2 ,t3 ,t4 ,t5 , t6));*/
